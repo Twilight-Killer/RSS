672469dcb224592e78bc8ce0bfcf73c46fd90d3b
Nik-Sch <nik.schelten@gmail.com>
2020-02-15 14:13:58 +0100

    now saves old feeds for 24hours

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import delay from 'delay';
 import Telegraf from "telegraf";
 import Parser from 'rss-parser';
 import { readFileSync } from 'fs';
+import { DateTime, Duration } from 'luxon';
 
+
+interface IItem {
+  time: DateTime;
+  link?: string;
+  title?: string;
+}
+const mapItem = (rssItem: Parser.Item): IItem => {
+  return {
+    time: DateTime.local(),
+    link: rssItem.link,
+    title: rssItem.title
+  }
+}
 interface IChannel {
   chatId: string | number;
   rssUrl: string;
-  rssItems: Parser.Item[] | undefined;
+  sentItems: IItem[] | undefined;
 }
 
 const channels: IChannel[] = JSON.parse(readFileSync('channels.json').toString());
@@ -15,31 +29,26 @@ const bot = new Telegraf('process.env.BOT_TOKEN');
 setInterval(async () => {
   for (const channel of channels) {
     const feed = await new Parser().parseURL(channel.rssUrl);
-    if (channel.rssItems === undefined) { // first rss download after start
+    if (channel.sentItems === undefined) { // first rss download after start
       console.log('first');
-      channel.rssItems = feed.items;
+      channel.sentItems = feed.items?.map(mapItem);
       return;
     }
-    if (!channel.rssItems || !feed.items) {
+    if (!channel.sentItems || !feed.items) {
       return;
     }
-    // console.log(feed.items.find((feedItem) => feedItem.guid === (channel.rssItems as any)[0].guid)?.link + '\n' + (channel.rssItems as any)[0].link);
-    const newItems = channel.rssItems.filter(item => !feed.items?.find((feedItem) => feedItem.link === item.link));
-    console.log(`${Date.now()} - diff: ${newItems.length}`)
+    const newItems = feed.items.filter(feedItem => !channel.sentItems?.find(channelItem => channelItem.link === feedItem.link)).map(mapItem);
+    console.log(`${DateTime.local()} new: ` + newItems.map(i => i.link + ', ') || 'none');
     for (const item of newItems) {
       bot.telegram.sendMessage(channel.chatId, `${item.title}\n${item.link}`);
     }
-    channel.rssItems = feed.items;
+    channel.sentItems = channel.sentItems.filter((item) => item.time.diffNow('hours').hours <= 24);
+    channel.sentItems.push(... newItems);
   }
-
 }, 5000);
 
 bot.telegram.getMe().then(botInfo => {
   bot.options.username = botInfo.username;
 });
-bot.start((ctx) => ctx.reply('Hello!'));
-
-bot.start((ctx) => ctx.reply('Hi'));
-
-// bot.telegram.sendMessage('@zeitonlinerss', 'Hello World!');
+bot.start((ctx) => ctx.reply(`last 24 hours: ${channels.length}`));
 bot.launch();
