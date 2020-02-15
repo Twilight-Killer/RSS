f8a921abe221d03e3db49227f23fd959424f0fc4
Nik-Sch <nik.schelten@gmail.com>
2020-02-15 13:35:22 +0100

    link instead of guid

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ interface IChannel {
 
 const channels: IChannel[] = JSON.parse(readFileSync('channels.json').toString());
 
-const bot = new Telegraf(process.env.BOT_TOKEN || '');
+const bot = new Telegraf('process.env.BOT_TOKEN');
 setInterval(async () => {
   for (const channel of channels) {
     const feed = await new Parser().parseURL(channel.rssUrl);
@@ -24,7 +24,7 @@ setInterval(async () => {
       return;
     }
     // console.log(feed.items.find((feedItem) => feedItem.guid === (channel.rssItems as any)[0].guid)?.link + '\n' + (channel.rssItems as any)[0].link);
-    const newItems = channel.rssItems.filter(item => !feed.items?.find((feedItem) => feedItem.guid === item.guid));
+    const newItems = channel.rssItems.filter(item => !feed.items?.find((feedItem) => feedItem.link === item.link));
     console.log(`diff: ${newItems.length}`)
     for (const item of newItems) {
       bot.telegram.sendMessage(channel.chatId, `${item.title}\n${item.link}`);
@@ -37,6 +37,7 @@ setInterval(async () => {
 bot.telegram.getMe().then(botInfo => {
   bot.options.username = botInfo.username;
 });
+bot.start((ctx) => ctx.reply('Hello!'));
 
 // bot.telegram.sendMessage('@zeitonlinerss', 'Hello World!');
 bot.launch();
